6c6c8c67b0c537613ff1710264446db551e231b3
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2021-05-25 16:29:13 +0200

    Added tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 class App extends React.Component {
     render () {
         return <div>
-            <p> Test </p>
+            <p>
+                test
+            </p>
         </div>
     }
 }
